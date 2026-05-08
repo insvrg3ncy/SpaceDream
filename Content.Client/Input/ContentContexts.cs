011497b69cfe0dc41d0eb626557e24908d874bce
insvrg3ncy <gunsshow9@gmail.com>
2026-05-08 11:16:55 +0300

    posing + echo port

--- Content.Client/Input/ContentContexts.cs
+++ Content.Client/Input/ContentContexts.cs
@@ -91,7 +91,7 @@ namespace Content.Client.Input
             human.AddFunction(ContentKeyFunctions.Arcade1);
             human.AddFunction(ContentKeyFunctions.Arcade2);
             human.AddFunction(ContentKeyFunctions.Arcade3);
-
+            human.AddFunction(ContentKeyFunctions.TogglePosing); // ADT tweak
             // actions should be common (for ghosts, mobs, etc)
             common.AddFunction(ContentKeyFunctions.OpenActionsMenu);
 
